0bd978482c4a7d2b4c01f4bc41f05c69604df05a
Argus Li <43020525+ArgusLi@users.noreply.github.com>
2026-07-24 12:53:26 -0700

    Refactor update config to use automatic retry of failed nodes for cluster fan out (#391)

--- apps/frontend/src/state/epics/rootEpic.ts
+++ apps/frontend/src/state/epics/rootEpic.ts
@@ -16,7 +16,6 @@ import {
   getCpuUsageEpic,
   getMemoryUsageEpic,
   monitorEpic,
-  saveMonitorSettingsEpic,
   metricsReadinessRetryEpic
 } from "./valkeyEpics"
 import { keyBrowserEpic } from "./keyBrowserEpic"
@@ -41,7 +40,6 @@ export const registerEpics = (store: Store) => {
     getCpuUsageEpic(),
     getMemoryUsageEpic(),
     monitorEpic(),
-    saveMonitorSettingsEpic(),
     metricsReadinessRetryEpic(store),
   ).subscribe({
     error: (err) => console.error("Epic error:", err),
